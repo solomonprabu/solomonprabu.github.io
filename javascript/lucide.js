e07b6654ddf05240664254db30da336c111a1012
Solomon Prabu <solomon.workspace@gmail.com>
2026-01-22 14:51:32 +0100

    update

--- javascript/lucide.js
+++ javascript/lucide.js
@@ -1,22 +1,22 @@
 // Initialize Lucide Icons
-lucide.createIcons();
+// lucide.createIcons();
 
-        // Smooth Scroll Logic for Navigation
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                // Prevent default hash behavior
-                e.preventDefault();
-                
-                // Get the target element by its ID
-                const targetId = this.getAttribute('href');
-                const targetElement = document.querySelector(targetId);
+// Smooth Scroll Logic for Navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        // Prevent default hash behavior
+        e.preventDefault();
 
-                if (targetElement) {
-                    // Smoothly scroll to the element
-                    window.scrollTo({
-                        top: targetElement.offsetTop - 70, // Adjust for fixed header height
-                        behavior: 'smooth'
-                    });
-                }
+        // Get the target element by its ID
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            // Smoothly scroll to the element
+            window.scrollTo({
+                top: targetElement.offsetTop - 70, // Adjust for fixed header height
+                behavior: 'smooth'
             });
-        });
+        }
+    });
+});
